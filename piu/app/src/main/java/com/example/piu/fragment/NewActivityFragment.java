98a5915fcbb528ae8c49ab4755c76a1d745d99aa
stfn19 <stefnsn97@gmail.com>
2019-12-12 21:21:32 +0200

    refactored activities in fragments & moved them in fragments folder

---
+++ piu/app/src/main/java/com/example/piu/fragment/NewActivityFragment.java
@@ -0,0 +1,27 @@
+package com.example.piu.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.piu.R;
+
+public class NewActivityFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_activity_new, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
