1cfc59c3f1e546a2283244098f5249e28f246c04
stfn19 <stefans9703@gmail.com>
2019-12-12 19:17:33 +0200

    refactored activities into fragments & moved them in fragment folder

---
+++ piu/app/src/main/java/com/example/piu/fragment/NewRuleFragment.java
@@ -0,0 +1,38 @@
+package com.example.piu.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.piu.R;
+
+public class NewRuleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_new_rule, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    //TODO: getMenuInflater not supported in Fragment???
+//    @Override
+//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//        if(v.getId()==R.id.devices_list){
+//            getMenuInflater().inflate(R.menu.add_remove_device_menu,menu);
+//        }
+//    }
+}
